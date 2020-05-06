7eb0cf694c1d7eeeae631f5dec5a2c57bbfa7d45
Andrew Kim <andrewswkim47@gmail.com>
2020-05-05 19:25:21 -0700

    add images

--- src/components/FakeMerchService.js
+++ src/components/FakeMerchService.js
@@ -1,28 +1,28 @@
 import Image1 from '../assets/shirt.jpg'
-import Image2 from '../assets/shirt.jpg'
-import Image3 from '../assets/shirt.jpg'
+import Image2 from '../assets/hoodie.jpg'
+import Image3 from '../assets/mask.png'
 import { v4 } from 'uuid';
 
 const masterMerchList = [
   {
     name: 'T-Shirt',
     img: Image1,
-    issue: 'This is a quality T-Shirt',
-    inventory: 10,
+    issue: 'Vintage Goods',
+    inventory: 5,
     id: v4()
   },
   {
-    name: 'Sweatshirt',
+    name: 'Hoodie',
     img: Image2,
-    issue: 'This is a quality Sweatshirt',
-    inventory: 10,
+    issue: 'Tribute to Mamba',
+    inventory: 24,
     id: v4()
   },
   {
-    name: 'Turtleneck',
+    name: 'Mask',
     img: Image3,
-    issue: 'This is a quality Turtleneck',
-    inventory: 10,
+    issue: 'Protect Yourself',
+    inventory: 20,
     id: v4()
   }
 ];
